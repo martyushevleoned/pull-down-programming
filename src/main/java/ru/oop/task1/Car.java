cccf91bc3dc3531cd77c95016ef8b49e375e37b8
Martyushev Leoned <lyonya.martyushev@mail.ru>
2024-04-06 10:22:39 +0500

    task 1

---
+++ src/main/java/ru/oop/task1/Car.java
@@ -0,0 +1,11 @@
+package ru.oop.task1;
+
+public class Car {
+
+    /**
+     * Перемещает человека на машине как можно ближе к точке назначения
+     */
+    public void moveTo(Person person, Position destination){
+
+    }
+}
