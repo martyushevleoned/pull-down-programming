6515e26e546cee6b4271d536d8c45560b8d88726
Martyushev Leoned <lyonya.martyushev@mail.ru>
2024-04-06 10:29:19 +0500

    task 2

---
+++ src/main/java/ru/oop/task2/Bicycle.java
@@ -0,0 +1,14 @@
+package ru.oop.task2;
+
+public class Bicycle implements Transport{
+
+    @Override
+    public Position getPosition() {
+        return null;
+    }
+
+    @Override
+    public void moveTo(Person person, Position destination) {
+
+    }
+}
